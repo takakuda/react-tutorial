7ad0c5697d420230287bfc4c6b401d3defaf7bff
kei <kei.takakuda.0415@icloud.com>
2018-07-01 17:57:45 +0900

    gulpfile作成

---
+++ client/gulpfile.js
@@ -0,0 +1,15 @@
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require("vinyl-source-stream");
+var reactify = require('reactify');
+
+gulp.task('browserify', function(){
+  var b = browserify({
+    entries: ['../client/script/index.js'],
+    transform: [reactify]
+  });
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./client/build'));
+});
